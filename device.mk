e0c4d419dfd421c4ca2247797eca2715288aa71c
goodmeow <harunbam3@gmail.com>
2022-12-03 07:12:31 +0000

    apollo: include firmware

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
+# Firmware
+$(call inherit-product, vendor/xiaomi/firmware/apollo/firmware.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Overlays
